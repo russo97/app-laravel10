5d7d996febd291a834d7c867c5724c90b02dee44
Wanderson Silva <wandersonsilva768@gmail.com>
2023-04-21 22:14:09 -0300

    docs: remoção dos controllers ainda não usados

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/contato', function () {
+    return view('site.contact');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
