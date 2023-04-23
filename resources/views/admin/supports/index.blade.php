7e23feb3801fea1e5e7c242933518d42b92ed59e
Wanderson Silva <wandersonsilva768@gmail.com>
2023-04-22 22:19:19 -0300

    feat: criação da view principal do módulo de suporte

---
+++ resources/views/admin/supports/index.blade.php
@@ -0,0 +1,3 @@
+<h1>
+    Listagem dos suportes
+</h1>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Site\SiteController;
+use App\Http\Controllers\Admin\SupportController;
+
+Route::get('/supports', [
+    SupportController::class, 'index'
+]) -> name('supports.index');
 
 Route::get('/contato', [SiteController::class, 'contact']) -> name('contato.index');
 
