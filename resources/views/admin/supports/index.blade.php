cc6d85861d19f463660e3846aa8df07e80a18a6d
Wanderson Silva <wandersonsilva768@gmail.com>
2023-04-22 23:10:39 -0300

    feat: criação da tabela responsável pela exibição do conteúdo vindo do banco de dados

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -3,5 +3,45 @@
 </h1>
 
 <a href="{{ route('supports.create') }}">
-    Criar novo suporte
+    Criar Dúvida
 </a>
+
+<table>
+    <thead>
+        <th>
+            Assunto
+        </th>
+
+        <th>
+            Status
+        </th>
+
+        <th>
+            Descrição
+        </th>
+
+        <th></th>
+    </thead>
+
+    <tbody>
+        @foreach($supports as $support)
+            <tr>
+                <td>
+                    {{ $support -> subject }}
+                </td>
+
+                <td>
+                    {{ $support -> status }}
+                </td>
+
+                <td>
+                    {{ $support -> body }}
+                </td>
+
+                <td>
+                    &gt;
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
